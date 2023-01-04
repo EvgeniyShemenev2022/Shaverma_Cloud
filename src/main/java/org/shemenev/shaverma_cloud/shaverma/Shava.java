37342d8c1fd539a56f071db4fd2b6af0f37acec3
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2023-01-05 00:20:41 +0300

    в классе Shava обеспечил проверку на непустое поле, а так же на минимальную длину полей и количество ингредиентов

--- src/main/java/org/shemenev/shaverma_cloud/shaverma/Shava.java
+++ src/main/java/org/shemenev/shaverma_cloud/shaverma/Shava.java
@@ -1,5 +1,7 @@
 package org.shemenev.shaverma_cloud.shaverma;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -7,7 +9,13 @@ import java.util.List;
 @Data
 public class Shava {
 
+
+    @NotNull
+    @Size(min=5, message="название должно быть не меньше 5 букв")
     private String name;
+
+    @NotNull
+    @Size(min=3, message="Вы должны выбрать не менее трех ингредиентов")
     private List<Ingredient> ingredients;
 }
 

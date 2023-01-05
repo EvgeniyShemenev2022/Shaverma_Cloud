e887ba53e7a444d7ac2527012c715b9e6eecf0c6
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2023-01-05 17:38:10 +0300

    в классе ShavaOrder обеспечил проверку полей формы

--- src/main/java/org/shemenev/shaverma_cloud/shaverma/ShavaOrder.java
+++ src/main/java/org/shemenev/shaverma_cloud/shaverma/ShavaOrder.java
@@ -1,6 +1,10 @@
 package org.shemenev.shaverma_cloud.shaverma;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,14 +12,30 @@ import java.util.List;
 @Data
 public class ShavaOrder {
 
+    @NotBlank(message="обязательное поле")
     private String deliveryName;
+
+    @NotBlank(message="обязательное поле")
     private String deliveryStreet;
+
+    @NotBlank(message="обязательное поле")
     private String deliveryCity;
+
+    @NotBlank(message="обязательное поле")
     private String deliveryRegion;
+
+    @NotBlank(message="обязательное поле")
     private String deliveryZip;
+
+    @CreditCardNumber(message="неверный номер карты")
     private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message="формат MM/YY")
     private String ccExpiration;
+
+    @Digits(integer=3, fraction=0, message="некорректный CVV")
     private String ccCVV;
+
     private List<Shava> shavaList = new ArrayList<>();
 
 
@@ -24,3 +44,4 @@ public class ShavaOrder {
     }
 
 }
+
